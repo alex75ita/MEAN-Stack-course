87e4f4d6dd9ac4b632d634ac5e780addc4cfe0d1
Alessandro Piccione <alex.75@tin.it>
2015-12-08 22:26:27 +0100

    Set Gulp watch

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require("gulp");
+var mocha = require("gulp-mocha");
+
+gulp.task("test", function() {
+	gulp
+		.src("./tests/*.js")
+		.pipe(mocha())
+		.on("error", function(error) {
+			this.emit("end");
+		})
+		;	
+});
+
+gulp.task("watch", function() {
+	gulp.watch("./**/*.js", ["test"]);
+});
