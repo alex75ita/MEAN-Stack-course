309c6942718af712858ab5b3855440479f25d48c
Alessandro Piccione <alex.75@tin.it>
2015-12-08 19:17:25 +0100

    Add filter and format on mocha test run

--- run tests.bat
+++ run tests.bat
@@ -1,2 +1,6 @@
 
-start node_modules\.bin\mocha tests\a_test.js
+.echo start node_modules\.bin\mocha tests\a_test.js
+.echo "-g '<regex>'"
+.echo "-R (nyan | xunit | dot)"
+
+start node_modules\.bin\mocha -R dot  -g "feature B" tests\* 
--- tests/a_test.js
+++ tests/a_test.js
@@ -1,6 +1,21 @@
 var assert = require("assert"); // use Node.js internal assert package
 
-describe("my feature", function() {
+describe("feature A", function() {
+	it("do_something", function (){
+		assert.equal("a", "a");		
+	});
+	
+	it("do_another_thing", function() {
+		assert.throws( function() {
+			throw "Error!";			
+			}	
+		);		
+	});
+	
+});
+
+
+describe("feature B", function() {
 	it("do_something", function (){
 		assert.equal("a", "a");		
 	});
