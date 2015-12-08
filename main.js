7c503a385bed0f45eb51db3aa3f155af7cad708f
Alessandro Piccione <alex.75@tin.it>
2015-12-08 17:24:40 +0100

    (refactoring) Create showData function.

--- main.js
+++ main.js
@@ -10,41 +10,56 @@ console.log("Hello World!");
 
 
 var uri = "mongodb://localhost:27017/timesheets";
-mongodb.MongoClient.connect(uri, function(error, db){
-	if(error){
-		console.error(error);
-		process.exit(1);		
-	}
-	
-	var person = Person("Alessandro");		
-	var doc = {"date": "2015-12-08", "name":person.name(), "type":"holiday", "hours":8};
-		
-	db.collection("records").insert(doc, 
-		function(error, result){
-			if(error){
-				console.error(error);
-				process.exit(1);
-			}			
-			
-			// show data
-			db.collection("records").find()/*.orderBy({"date":-1})*/.limit(5).toArray(
-				function(error, docs){
-					if(error){
-						console.error(error);
-						process.exit(1);
-					}		
-					
-					console.log("Last records:");
-					docs.forEach(
-						function(doc){
-							console.log(JSON.stringify(doc));							
-						}
-					);
-					
-					process.exit(0);
-				}				
-			);
+
+function run(showDataCallback) {
+	mongodb.MongoClient.connect(uri, function(error, db){
+		if(error) {
+			console.error(error);
+			process.exit(1);		
 		}
-	);
 		
-});
+		var person = Person("Alessandro");		
+		var doc = {"date": "2015-12-08", "name":person.name(), "type":"holiday", "hours":8};
+			
+		db.collection("records").insert(doc, 
+			function(error, result) {
+				if(error) {
+					console.error(error);
+					process.exit(1);
+				}			
+				
+				showDataCallback && showDataCallback(db);
+			}
+		);
+			
+	});
+
+}
+
+function showData(db) {
+	
+	//mongodb.MongoClient.connect(uri, function(error, db){
+	
+		db.collection("records").find()/*.orderBy({"date":-1})*/.limit(5).toArray(
+			function(error, docs) {
+				if(error){
+					console.error(error);
+					process.exit(1);
+				}		
+				
+				console.log("Last records:");
+				docs.forEach(
+					function(doc){
+						console.log(JSON.stringify(doc));							
+					}
+				);
+				
+				process.exit(0);
+			}				
+		);		
+	
+	//});	
+};
+
+
+run(showData);
