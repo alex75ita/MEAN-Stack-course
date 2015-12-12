f7c5697c44d530afe376978491b73a1a425fb0a7
Alessandro Piccione <alex.75@tin.it>
2015-12-13 00:39:15 +0100

    Add .kinds property that contains valida values. Enhanced test on Item.

--- entities/item_test.js
+++ entities/item_test.js
@@ -2,6 +2,7 @@ var entities = require("../entities");
 var should = require("should");
 
 describe("Item", function() {
+	
 	it("Item() should create a valid instance", function() {
 		var item = entities.Item();		
 		should.exists(item);
@@ -9,6 +10,39 @@ describe("Item", function() {
 		item.should.have.property("person");
 	});
 	
+	describe(".kind", function() {
+		it("should be set to \"holiday\" as default", function() {
+			var item = entities.Item();
+			should(item).have.property("kind");
+			item.kind.should.be.exactly("holiday");
+		});
+		
+		it("should be set to \"holiday\"", function() {
+			var item = entities.Item();
+			item.kind = item.kinds.holiday;
+			
+			item.kind.should.be.exactly("holiday");
+		});
+		
+		it("should be set to \"permit\"", function() {
+			var item = entities.Item();
+			item.kind = item.kinds.permit;
+			
+			item.kind.should.be.exactly("permit");
+		});		
+	});
+	
+	
+	describe(".kinds", function() {
+		it("must contains \"holiday\" and \"permit\"", function() {
+			var item = entities.Item();
+			should(item).have.property("kinds");
+			item.kinds.should.be.type("object");
+			should(item.kinds).have.property("holiday");
+			should(item.kinds).have.property("permit");
+		});
+	});
+			
 	describe(".date", function() {
 		it("should return a date", function() {
 			var year = 2015;
@@ -25,5 +59,7 @@ describe("Item", function() {
 			should(date.getDate()).be.exactly(dateNumber);	
 		});
 	});
+	
+	
 	
 });
