62e23093e3a176e10d659c81adabe6fb75d1473f
Alessandro Piccione <alex.75@tin.it>
2015-12-13 00:53:47 +0100

    Improve the test of Person

--- entities/person_test.js
+++ entities/person_test.js
@@ -2,26 +2,35 @@ var should = require("should");
 var entities = require("../entities"); // = entities/index.js
 
 describe("Person", function() {
-	it("Person() work", function() {	
-		var name = "Ezechiele";	
-		var p = entities.Person(name);
-		should.exists(p);
-		p.should.have.property("name", name);
-		//p.name.should.be.exactly(name);
+	
+	describe("Person()", function() {
+		
+		it("should create a valid Person", function() {	
+			var name = "Ezechiele";	
+			var p = entities.Person(name);
+			should.exists(p);
+			p.should.have.property("name", name);
+			//p.name.should.be.exactly(name);
+		});
+				
+		it('when "name" is not given has "name" equal to "undefined"', function() {
+			var person = entities.Person();
+			person.name.should.be.exactly("undefined");		
+		});
+		
 	});	
 	
-	it("name() should change name", function() {
-		var newName = "Albert";
-		var p = entities.Person("aaa");
+	describe(".name", function() {
+		it("should change name", function() {
+			var newName = "Albert";
+			var p = entities.Person("aaa");
 		
-		p.name = newName;
-		p.name.should.be.exactly(newName);		
+			p.name = newName;
+			p.name.should.be.exactly(newName);
+		});		
 	});
 	
-	it('Person() when "name" is not given has "name" equal to "undefined"', function() {
-		var person = entities.Person();
-		person.name.should.be.exactly("undefined");		
-	});
+
 
 });
 
