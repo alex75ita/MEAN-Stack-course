dbe8ec21bef2a62742702e48eef3e587445305b2
Alessandro Piccione <alex.75@tin.it>
2015-12-17 23:52:58 +0100

    Add disconnection to mongo before every tests

--- tests/mongoose schemas_test.js
+++ tests/mongoose schemas_test.js
@@ -10,20 +10,18 @@ var mongoDBConnection = "mongodb://localhost:27017/timesheets_test";
 
 describe("Mongoose", function() {
 	
-
+	beforeEach(function(done) {
+		// close connection
+		if(mongoose.connection.db) {
+			//console.log("...close conenction");
+			mongoose.disconnect( function() { done(); });
+			//test.fail("connection is open");
+		}
+		else 
+			done();			
+	});
 	
-	describe("connect()", function() {
-		
-		beforeEach(function(done) {
-			// close connection
-			if(mongoose.connection.db) {
-				//console.log("...close conenction");
-				mongoose.disconnect( function() { done(); });
-				//test.fail("connection is open");
-			}
-			else 
-				done();			
-		});
+	describe("connect()", function() {	
 		
 		it("doesn't raise error", function() {
 			mongoose.connect(mongoDBConnection, function(error) {
@@ -32,39 +30,68 @@ describe("Mongoose", function() {
 			
 		});
 		
-		it("when URI is wrong ", function() {
+		// todo: this cause timeout
+		it.skip("when URI is wrong ", function(done) {
 			var wrong_uri = "mongodb://fake:27017/a_collection";
-			mongoose.connect(wrong_uri, function(error) {
+			mongoose.connect(wrong_uri, function(error) {				
 				should(error).be.ok;
 			});
 		});
 		
 	});
 	
-	describe.skip("User.save()", function() {
+	describe("User.save()", function() {
 		it("should save document in collection", function() {
 			
 			var name = "Alex";
 			
-			// todo: add cleaning
-			mongoose.disconnect();
 			
-			
+			// todo: add cleaning	
+			//deleteUser(name, function() {		
+						
 			mongoose.connect(mongoDBConnection, function() {
+				
+				//mongoose.connection.db.dropDatabase( function() {					
 			
-				var user = new User({
-					name: name,
-					email: "alex_1@gmail.com"				
-				});
-			
-				user.save(function(error) {
-					console.error("fail to save: " + error);
+					var user = new User({
+						name: name,
+						email: "alex_1@gmail.com"				
+					});
 					
-					//var users = loadUser(name
-					
-					//should(user).not.be.ok;									
-				});
-			}); // connect
+					console.log(111);
+				
+					user.save(function(error) {
+						console.log(222);
+						console.error("fail to save: " + error);
+						
+						User.find({"name":name}, function(error, docs) {
+							console.log(333);
+							should(error).not.be.ok;	
+							if(error)
+								test.fail(error);
+							console.log(docs);							
+						});	
+						
+						/*
+						loadUsers(name, function(error, docs) {
+							console.log(333);
+							if(error)
+								test.fail(error);
+							console.log(docs);
+						});*/
+												
+							/* var promise = gnr.save();
+							assert.ok(promise instanceof require('mpromise'));
+						
+							promise.then(function(doc) {
+							assert.equal(doc.name, "Guns N' Roses");
+							});*/						
+						
+						//should(user).not.be.ok;									
+					});
+				//}); //dropDatabase
+				
+			}); //connect
 			
 		});
 		
@@ -76,15 +103,13 @@ describe("Mongoose", function() {
 
 // utils
 
-function loadUser(name, errorCB, doneCB) {
-	
-	mongoose.disconnect();
-	
-	mongoose.connect(mongoDBConnection, function() {
-		User.find({"name":name}, function(error, docs) {
-			if(error)
-				errorCB(error);
-			doneCB(docs);
-		});		
-	});		
+function loadUser(name, callback) {	
+	User.find({"name":name}, callback);	
 }
+
+/*
+function deleteUser(namee, done) {
+    User.model.remove({}, function() {
+      done();
+    });
+}*/
