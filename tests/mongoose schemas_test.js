0a4d9fa9c256e9c61c3ee73aace7eb2362f98116
Alessandro Piccione <alex.75@tin.it>
2015-12-21 23:02:23 +0100

    Improve tests. (WIP) connection on wrong URL fail for test timeout instead of connection fail.

--- tests/mongoose schemas_test.js
+++ tests/mongoose schemas_test.js
@@ -10,49 +10,72 @@ var mongoDBConnection = "mongodb://localhost:27017/timesheets_test";
 
 describe("Mongoose", function() {
 	
-	beforeEach(function(done) {
+    /*
+	beforeEach.skip(function(done) {
 		// close connection
+		console.log("mongoose.connection.db?");
 		if(mongoose.connection.db) {
-			//console.log("...close conenction");
-			mongoose.disconnect( function() { done(); });
-			//test.fail("connection is open");
+			console.log("open");
+			mongoose.disconnect( function() {
+                console.log("disconnect callback");
+				done();
+			});
+            
+            if(mongoose.connection.db)
+                console.log("alredy connected");
 		}
-		else 
-			done();			
-	});
-	
+		else {
+			console.log("closed");
+			done();
+		}			
+	});*/
+    
+    beforeEach(function() {
+         mongoose.disconnect();        
+    });
+    
+
 	describe("connect()", function() {	
-		
-		it("doesn't raise error", function() {
-			mongoose.connect(mongoDBConnection, function(error) {
-				should(error).not.be.ok;	
-			});
-			
-		});
-		
-		// todo: this cause timeout
-		it.skip("when URI is wrong ", function(done) {
-			var wrong_uri = "mongodb://fake:27017/a_collection";
-			mongoose.connect(wrong_uri, function(error) {				
-				should(error).be.ok;
-			});
-		});
+				
+        it("doesn't raise error", function(done) {
+            mongoose.connect(mongoDBConnection);             
+            var db = mongoose.connection;
+            db.on("error", function(error){
+                //console.log(test.assert.fail);
+                test.assert.fail("connection error");
+                done();           
+            });            
+            db.on("open", function() { 
+                done();                
+            });  
+		});                
+   
+        // this one goes in timeout
+		describe("when URI is wrong ", function() {
+            it("raise error", function(done) {
+                var wrong_uri = "mongodb://fake_server:27017/a_collection";
+                mongoose.connect(wrong_uri);
+              
+                var db = mongoose.connection;
+                db.on("error", function(error) { 
+                    //console.log("Error: " + error);   
+                    should(error).have.property("name");
+                    should(error).have.property("message");
+                    done();    
+                });	
+            });
+		});     
 		
 	});
 	
 	describe("User.save()", function() {
 		it("should save document in collection", function() {
 			
-			var name = "Alex";
-			
+			var name = "Alex";			
 			
 			// todo: add cleaning	
 			//deleteUser(name, function() {		
-						
 			mongoose.connect(mongoDBConnection, function() {
-				
-				//mongoose.connection.db.dropDatabase( function() {					
-			
 					var user = new User({
 						name: name,
 						email: "alex_1@gmail.com"				
@@ -102,6 +125,10 @@ describe("Mongoose", function() {
 
 
 // utils
+
+function conenctToDb(callback) {
+    //mongoose.connect(mongoDBConnection, function() {
+}
 
 function loadUser(name, callback) {	
 	User.find({"name":name}, callback);	
